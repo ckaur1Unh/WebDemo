4eff78e389593edbdc599024a598883115cf16a0
ckaur1Unh <119720662+ckaur1Unh@users.noreply.github.com>
2022-12-06 21:55:41 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const http= require('http');
 const fs= require('fs');
 const path = require('path');
-const server   =http.createServer((req,res) => {
+const {MongoClient} = require('mongodb');
+const uri ="mongodb+srv://chan:chan@cluster0.zu4ucta.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
+
+const DBconnect=async()=>{
+    try{
+        await client.connect();
+        console.log("db connected")
+    
+    }
+    catch(e){
+        console.log(e)
+    }
+}
+DBconnect();
+const server   =http.createServer(async(req,res) => {
 const headers = {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "OPTIONS, POST, GET",
@@ -21,15 +36,13 @@ if(req.url === '/'){
 }
 else if(req.url=='/api')
 {
-    fs.readFile( path.join(__dirname,'public','db.json'),(err,data)=>{
-
-        if (err) throw err;
-
-        res.writeHead(200,headers);
-		
-		
-        res.end(data);
-        })
+  
+    const cursor = client.db("campus").collection("project").find({});
+    const results = await cursor.toArray();
+    //console.log(results);
+    const js= (JSON.stringify(results));
+    console.log(js);
+    res.end(js);
 
 }
 else{
